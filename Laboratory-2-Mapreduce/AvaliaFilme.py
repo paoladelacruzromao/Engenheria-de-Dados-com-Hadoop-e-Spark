427ca223c1fd2312d6ac61ba50c78690960675a9
paoladelacruzromao <87387315+paoladelacruzromao@users.noreply.github.com>
2021-10-25 19:38:41 -0300

    Add files via upload

---
+++ Laboratory-2-Mapreduce/AvaliaFilme.py
@@ -0,0 +1,14 @@
+from mrjob.job import MRJob
+
+class MRAvaliaFilme(MRJob):
+    def mapper(self, key, line):
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield rating, 1
+
+    def reducer(self, rating, occurences):
+        yield rating, sum(occurences)
+
+if __name__ == '__main__':
+    MRAvaliaFilme.run()
+
+
